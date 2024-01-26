d7c378a9bd4faebcc96fa6273e25444eedc8a1fe
Ali Uğur <aliugurtalay16@gmail.com>
2024-01-27 02:25:28 +0300

    Add files

--- README.md
+++
@@ -1,2 +0,0 @@
-# bistScanner
-Scan BIST daily and send notification to telegram group that consist of golden cross stocks
---
+++ main.py
@@ -0,0 +1,50 @@
+import time
+import telebot
+import os 
+from dotenv import load_dotenv
+from scraper import StockDataFetcher
+import schedule
+from workalendar.europe import Turkey
+from datetime import date
+import threading
+
+def is_today_working_day():
+    cal = Turkey()
+    return cal.is_working_day(date.today())
+
+
+load_dotenv()
+fetcher = StockDataFetcher()
+bot = telebot.TeleBot(os.getenv("API_KEY"))
+
+def scheduled_job():
+    if not is_today_working_day():
+        bot.send_message(os.getenv("CHAT_ID"), "Bugün tatil, iyi tatiller :)")
+        return
+    current_date = time.strftime("%d/%m/%Y")
+    stock_names = fetcher.fetch_data()
+    bot.send_message(os.getenv("CHAT_ID"), current_date)
+    for stock_name in stock_names:
+        bot.send_message(os.getenv("CHAT_ID"), stock_name)
+        
+        
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    bot.reply_to(message, "Merhaba, bu bot sana her akşam saat 8'de BİST'de goldencross'a başlayan hisseleri gösterir.")
+
+schedule.every(5).seconds.do(scheduled_job)
+
+def start_pooling():
+    while True:
+        bot.polling(non_stop=True)
+        
+def start_scheduling():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+        
+pooling_thread = threading.Thread(target=start_pooling)
+pooling_thread.start()
+
+scheduling_thread = threading.Thread(target=start_scheduling)
+scheduling_thread.start()
